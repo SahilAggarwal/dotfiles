9f37e1a87307dad2f86d3e7fe62ed774bd7c7605
Sahil Aggarwal <sahil.agg15@gmail.com>
2018-02-04 01:11:13 +0530

    Adds git and openssh in package list

--- install_packages.sh
+++ install_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-packages=(alsa-utils xmonad xmonad-contrib xmobar stalonetray gnome-terminal xorg xorg-xsetroot)
+packages=(alsa-utils xmonad xmonad-contrib xmobar stalonetray gnome-terminal xorg xorg-xsetroot git openssh)
 
 for package in ${packages[*]}:
 do
