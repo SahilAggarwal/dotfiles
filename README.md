33ce4d43935e0c8000e8e58518a6b9284cdb4985
Sahil Aggarwal <sahil.agg15@gmail.com>
2018-02-04 09:28:07 +0530

    Ads all xorg packages

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # dotfiles
 
 All dotfiles required to setup xmonad and xmobar
+
+### Webcam
+
+https://github.com/patjak/bcwc_pcie/wiki/Get-Started#firmware-extraction
--- install_packages.sh
+++ install_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-packages=(alsa-utils xmonad xmonad-contrib xmobar stalonetray gnome-terminal xorg xorg-xsetroot git openssh)
+packages=(alsa-utils xmonad xmonad-contrib xmobar stalonetray gnome-terminal git openssh xorg-luit xorg-server xorg-server-common xorg-server-xwayland xorg-setxkbmap xorg-xauth xorg-xbacklight xorg-xhost xorg-xinit xorg-xkbcomp xorg-xmodmap xorg-xrandr xorg-xrdb xorg-xset xorg-xsetroot)
 
 for package in ${packages[*]}:
 do
